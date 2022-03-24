56b85cf952f59bdf240ba38c6a6d4207e673a126
dr0seph <94659939+dr0seph@users.noreply.github.com>
2022-03-24 15:39:45 -0400

    Add files via upload

---
+++ Bright_Ideas-main/App/flask_app/models/idea.py
@@ -0,0 +1,71 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import user
+
+class Idea:
+    db_name = 'Bright_Ideas_App'
+    def __init__(self,db_data):
+        self.id = db_data['id']
+        self.content = db_data['content']
+        self.sender_id = db_data['sender_id']
+        self.created_at = db_data['created_at']
+        self.updated_at = db_data['updated_at']
+        self.liked_by=[]
+        self.tot_likes=[]
+        self.username=[]
+
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO ideas (content,sender_id) VALUES (%(content)s,%(sender_id)s);"
+        return connectToMySQL(cls.db_name).query_db(query,data)
+
+    @classmethod
+    def destroy(cls,data):
+        query = "DELETE FROM ideas WHERE ideas.id = %(id)s;"
+        return connectToMySQL(cls.db_name).query_db(query,data)
+    
+    @classmethod
+    def get_all_ideas(cls):
+        query = "SELECT ideas.id as id, ideas.content as content, users.name as sender, users.id AS sender_id, likes.user_id AS liker_id ,count(likes.user_id) as tot_likes FROM ideas JOIN users ON sender_id = users.id LEFT JOIN likes ON idea_id = ideas.id GROUP BY ideas.id ORDER BY tot_likes desc;"
+        results = connectToMySQL(cls.db_name).query_db(query)
+        print (results)
+        # ideas = []
+        # for row in results:
+        #     cls.tot_likes=row['tot_likes']
+        #     cls.username=row['name']
+        #     ideas.append(cls(row))
+        return results
+
+    @classmethod
+    def get_this_idea(cls,data):
+        query = "SELECT * FROM ideas LEFT JOIN likes ON ideas.id = likes.idea_id LEFT JOIN users ON users.id=likes.user_id WHERE ideas.id = %(id)s;"
+        results = connectToMySQL(cls.db_name).query_db(query,data)
+        idea = cls(results[0])
+        for row in results:
+            # # if there are no favorites
+            # if row['users.id'] == None:
+            #     break
+            # common column names come back with specific tables attached
+            data = {
+                "id": row['users.id'],
+                "name": row['name'],
+                "alias": row['alias'],
+                "email": row['email'],
+                "password_un": row['password_un'], #unhashed
+                "password": row['password'], #hashed
+                "created_at": row['users.created_at'],
+                "updated_at": row['users.updated_at']
+            }
+            idea.liked_by.append(user.User(data))
+        return idea
+
+
+    @classmethod
+    def like_idea(cls,data):
+        query = "INSERT INTO likes (user_id,idea_id) VALUES (%(user_id)s,%(idea_id)s);"
+        return connectToMySQL(cls.db_name).query_db(query,data)
+
+
+    @classmethod
+    def unlike_idea(cls,data):
+        query = "DELETE FROM likes WHERE user_id = %(user_id)s and idea_id = %(idea_id)s;"
+        return connectToMySQL(cls.db_name).query_db(query,data)
